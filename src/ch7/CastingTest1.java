4e2ba86fd6b9f608f8e41b70e6fc65a9b9f585c4
gyuri <rbfl6418@gmail.com>
2021-04-07 12:01:53 +0900

    Revert "다형성 예제 실습"
    This reverts commit 881eb47a824b96bb2e26f88280ca8ddf92a00f99.

--- src/ch7/CastingTest1.java
+++ src/ch7/CastingTest1.java
@@ -8,7 +8,6 @@ public class CastingTest1 {
 		FireEngine fe2 = null;
 		
 		car = fe;
-		System.out.println(car instanceof FireEngine); //true
 		//car.water(); -> 컴파일 에러  
 		fe2 = (FireEngine)car; // 얘도 부모 -> 자식으로 형변환인데 왜 되는 것?
 		fe2.water();
@@ -28,24 +27,9 @@ class Car{
 	void stop() {
 		System.out.println("stop!!!");
 	}
-	
-	void doWork(Car c) {
-		if(c instanceof FireEngine) {
-			FireEngine fe = (FireEngine)c;
-			fe.water();
-		}else if(c instanceof Ambulance) {
-			Ambulance a = (Ambulance)c;
-			a.siren();
-		}
-	}
 }
 class FireEngine extends Car{
 	void water() {
 		System.out.println("water!!!");
-	}
-}
-class Ambulance extends Car{
-	void siren() {
-		System.out.println("woo-woo");
 	}
 }
--- src/ch7/CastingTest2.java
+++ src/ch7/CastingTest2.java
@@ -8,10 +8,9 @@ public class CastingTest2 {
 		FireEngine fe = null;
 		
 		car.drive();
-		System.out.println(car instanceof FireEngine); //false
-		fe = (FireEngine)car; //컴파일 - OK, 실행 시(런타임) 에러 발생  
+		fe = (FireEngine)car; //컴파일 - OK, 실행 시 에러 발생  
 		fe.drive();
-		car2 = fe;                                       
+		car2 = fe;
 		car2.drive();
 	}
 
